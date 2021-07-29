f8f8d55534e37358ec8836b0890ad03c56f51b3b
xu.sun <xu.sun@weimob.com>
2021-07-29 23:56:03 +0800

    climbingStairs

---
+++ src/main/java/com/itsnine/code/simple/ArrayFindRepeatNum.java
@@ -0,0 +1,21 @@
+package com.itsnine.code.simple;
+
+import java.util.HashMap;
+
+/**
+ * 寻找数组中重复的数字
+ * @author xu.sun
+ * @create 2021-07-29-10:19 下午
+ */
+public class ArrayFindRepeatNum {
+    public int findNum(int[] nums){
+        HashMap<Integer, Integer> numMap = new HashMap<>();
+        for (int num : nums) {
+            if (numMap.containsKey(num)){
+                return num;
+            }
+            numMap.put(num,num);
+        }
+        return -1;
+    }
+}
---
+++ src/main/java/com/itsnine/code/simple/ClimbingStairs.java
@@ -0,0 +1,82 @@
+package com.itsnine.code.simple;
+
+/**
+ * 青蛙跳台阶
+ *
+ * @author xu.sun
+ * @create 2021-07-29-11:01 下午
+ */
+public class ClimbingStairs {
+    /**
+     * 青蛙一次可以跳1级台阶也可以跳2级台阶，一共有n级台阶，共有多少中跳法
+     *
+     * <p>
+     * 跳n级台阶可看作f(n)
+     * 第一次跳有两种选择：
+     * 跳1级，后面剩下n-1级台阶，可看作f(n-1)
+     * 跳2级，后面剩下n-2级台阶，可看作f(n-2)
+     * f(n)=f(n-1)+f(n-2)
+     * </p>
+     *
+     * @param n
+     * @return
+     */
+    public int numWays1(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n == 1 || n == 2) {
+            return n;
+        }
+        int n1 = 1;
+        int n2 = 2;
+        int sum = 0;
+        for (int i = 2; i < n; i++) {
+            sum = (n1 + n2) % 1000000007;
+            n1 = n2;
+            n2 = sum;
+        }
+        return sum;
+    }
+
+    /**
+     * 青蛙跳台阶，一次可以跳1级，一次也可以跳n级，共有n级台阶，共多少中跳法
+     * 跳n级台阶的跳法可以看作f(n)
+     * <p>
+     * 当第一次跳1级，后面剩下n-1级台阶，可看作f(n-1)
+     * 当第一次跳2级，后面剩下n-2级台阶，可看作f(n-2)
+     * ....
+     * 当第一次跳n-1级，后面剩下1级台阶，可看作f(n-(n-1))=f(1)
+     * 当第一次跳n级，后面剩下0级台阶，可看作f(n-n)=f(0)
+     * f(n) = f(n-1)+f(n-2)+....+f(1)+f(0)
+     * f(n-1) = f(n-2)+f(n-3)+....+f(1)+f(0)
+     * 两者相减
+     * f(n) = 2*f(n-1)
+     *
+     * </p>
+     *
+     * @param n
+     * @return
+     */
+    public int numWays2(int n) {
+        //自顶向下
+        /**
+         * if (n <= 1) {
+         return 1;
+         }
+         return 2 * numWays2(n - 1);
+         */
+        //自底向上
+        if (n <= 1) {
+            return n;
+        }
+        int a = 1;
+        int tmp = 1;
+        for (int i = 2; i <= n; i++) {
+            tmp = 2 * a;
+            a = tmp;
+        }
+        return tmp;
+    }
+
+}
